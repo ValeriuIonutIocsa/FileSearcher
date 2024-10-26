53ffd8a2d343ab72fe824d549cf5b82f3a3b3df7
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2024-10-26 16:37:45 +0300

    added delete and copy context menu options

--- Projects/Personal/FileSearcher/FileSearcher/src/main/java/com/personal/scripts/file_search/CustomTableCellSearchResultFileName.java
+++ Projects/Personal/FileSearcher/FileSearcher/src/main/java/com/personal/scripts/file_search/CustomTableCellSearchResultFileName.java
@@ -1,12 +1,21 @@
 package com.personal.scripts.file_search;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.personal.scripts.file_search.workers.search.SearchResult;
+import com.utils.gui.alerts.CustomAlertException;
 import com.utils.gui.clipboard.ClipboardUtils;
 import com.utils.gui.objects.tables.table_view.CustomTableCell;
 import com.utils.io.IoUtils;
+import com.utils.io.file_deleters.FactoryFileDeleter;
 
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 
 class CustomTableCellSearchResultFileName extends CustomTableCell<SearchResult, Object> {
 
@@ -30,6 +39,14 @@ class CustomTableCellSearchResultFileName extends CustomTableCell<SearchResult, 
 			final MenuItem selectInExplorerMenuItem = new MenuItem("select in explorer");
 			selectInExplorerMenuItem.setOnAction(event -> selectInExplorer(searchResult));
 			contextMenu.getItems().add(selectInExplorerMenuItem);
+
+			final MenuItem deleteMenuItem = new MenuItem("delete");
+			deleteMenuItem.setOnAction(event -> delete());
+			contextMenu.getItems().add(deleteMenuItem);
+
+			final MenuItem copyMenuItem = new MenuItem("copy");
+			copyMenuItem.setOnAction(event -> copy());
+			contextMenu.getItems().add(copyMenuItem);
 		}
 		return contextMenu;
 	}
@@ -47,5 +64,45 @@ class CustomTableCellSearchResultFileName extends CustomTableCell<SearchResult, 
 		final String filePathString = searchResult.createFilePathString();
 		final String appFolderPathString = FileSearcherUtils.createAppFolderPathString();
 		IoUtils.selectFileInExplorer(filePathString, appFolderPathString);
+	}
+
+	private void delete() {
+
+		try {
+			final TableView<SearchResult> tableView = getTableView();
+			final List<SearchResult> selectedItemList = tableView.getSelectionModel().getSelectedItems();
+			for (final SearchResult selectedItem : selectedItemList) {
+
+				final String filePathString = selectedItem.createFilePathString();
+				FactoryFileDeleter.getInstance().deleteFile(filePathString, true, true);
+			}
+
+		} catch (final Exception exc) {
+			new CustomAlertException("error", "error occurred while deleting files", exc).showAndWait();
+		}
+	}
+
+	private void copy() {
+
+		try {
+			final List<File> fileList = new ArrayList<>();
+			final TableView<SearchResult> tableView = getTableView();
+			final List<SearchResult> selectedItemList = tableView.getSelectionModel().getSelectedItems();
+			for (final SearchResult selectedItem : selectedItemList) {
+
+				final String filePathString = selectedItem.createFilePathString();
+				final File file = new File(filePathString);
+				fileList.add(file);
+			}
+
+			final ClipboardContent clipboardContent = new ClipboardContent();
+			clipboardContent.putFiles(fileList);
+
+			final Clipboard clipboard = Clipboard.getSystemClipboard();
+			clipboard.setContent(clipboardContent);
+
+		} catch (final Exception exc) {
+			new CustomAlertException("error", "error occurred while copying files", exc).showAndWait();
+		}
 	}
 }
